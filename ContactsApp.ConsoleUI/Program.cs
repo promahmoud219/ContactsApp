d4f22cd008af48c609e18565f7e1354b28f8dc48
pro mahmoud ahmed <promahmoud219@gmail.com>
2026-02-07 17:17:34 +0200

    chore: update startup and project references

--- ContactsApp.ConsoleUI/Program.cs
+++ ContactsApp.ConsoleUI/Program.cs
@@ -1,19 +1,45 @@
-﻿using ContactsApp.Infrastructure.Data;
-using ContactsApp.ConsoleUI.Features.MainMenu;
-using ContactsApp.Core.Contacts.Interfaces;
+﻿using ContactsApp.Core.Contacts.Interfaces;
+using ContactsApp.Core.Contacts.UseCases.AddContact;
+
+using ContactsApp.Infrastructure.Data;
 using ContactsApp.Infrastructure.Repositories;
 
-namespace ContactsApp
-{
-    internal class Program
-    {
+using ContactsApp.ConsoleUI;
+using ContactsApp.ConsoleUI.Application; 
+using ContactsApp.ConsoleUI.Shared; 
+using ContactsApp.ConsoleUI.Features.AddContact;
+using ContactsApp.ConsoleUI.Features.MainMenu;
 
-        static void Main(string[] args)
-        {
-            DatabaseInitializer.Initialize();
-            IContactRepository repository = new ContactRepository();
-            MainMenuController ctr = new MainMenuController(repository);
-            ctr.Run();
-        }
+using Microsoft.Extensions.DependencyInjection;
+
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+        DatabaseInitializer.Initialize();
+
+        var services = new ServiceCollection();
+
+        // Core / Infra
+        services.AddTransient<IContactRepository, ContactRepository>();
+        services.AddTransient<IAddContactUseCase, AddContactUseCase>();
+
+        // AddContact feature (Console)
+        services.AddTransient<AddContactController>();
+        services.AddTransient<IAddContactOutputPort, AddContactOutputPort>();
+        services.AddTransient<AddContactView>();
+
+        services.AddTransient<MainMenuView>();
+
+        services.AddSingleton<ApplicationController>();
+
+        // Shared
+        services.AddTransient<IShowMessage, ConsoleMessageView>();
+
+        var provider = services.BuildServiceProvider();
+
+        var app = provider.GetRequiredService<ApplicationController>();
+        app.Run();
     }
 }
